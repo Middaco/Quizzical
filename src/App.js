c01dd42147ee260278dfe5db0c033f3ff7a9e5a2
Middaco <bogdan02081@gmail.com>
2024-10-17 10:24:56 +0300

    Created the start page of Quizzical

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <h1>Quizzical</h1>
+      <p>Are you ready to prove your knowledge?</p>
+      <button className="quizz-btn">Start Quizzical!</button>
     </div>
   );
 }
