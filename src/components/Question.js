18b8c6ab68ba386bc559311655e0cb1b01196b73
Middaco <bogdan02081@gmail.com>
2024-10-18 17:42:09 +0300

    made answers display in random order

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,7 @@
 export default function Question(props){
     function handleClick(){}
+    const answers = props.incorrect_answers
+    answers.splice(Math.floor(Math.random() * answers.length), 0, props.correct_answer)
     return (
         <div className="question-container">
             <h2>{props.question}</h2>
@@ -7,24 +9,26 @@ export default function Question(props){
             <button 
                 onClick={handleClick}
                 className ="question-option"
-            >{props.correct_answer}</button>
-            <button 
-                onClick={handleClick}
-                className ="question-option"
             >
-                {props.incorrect_answers[0]}
+                {answers[0]}
             </button>
             <button 
                 onClick={handleClick}
                 className ="question-option"
             >
-                {props.incorrect_answers[1]}
+                {answers[1]}
             </button>
             <button 
                 onClick={handleClick}
                 className ="question-option"
             >
-                {props.incorrect_answers[2]}
+                {answers[2]}
+            </button>
+            <button 
+                onClick={handleClick}
+                className ="question-option"
+            >
+                {answers[3]}
             </button>
             <hr/>
         </div>
