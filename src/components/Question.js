aa51fcf1987ed179f455ca0f072d36428703d7c4
Middaco <bogdan02081@gmail.com>
2024-10-18 17:34:06 +0300

    Done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,31 @@
 export default function Question(props){
+    function handleClick(){}
     return (
         <div className="question-container">
             <h2>{props.question}</h2>
 
             <button 
+                onClick={handleClick}
                 className ="question-option"
             >{props.correct_answer}</button>
-            <button className ="question-option">{props.incorrect_answers[0]}</button>
-            <button className ="question-option">{props.incorrect_answers[1]}</button>
-            <button className ="question-option">{props.incorrect_answers[2]}</button>
+            <button 
+                onClick={handleClick}
+                className ="question-option"
+            >
+                {props.incorrect_answers[0]}
+            </button>
+            <button 
+                onClick={handleClick}
+                className ="question-option"
+            >
+                {props.incorrect_answers[1]}
+            </button>
+            <button 
+                onClick={handleClick}
+                className ="question-option"
+            >
+                {props.incorrect_answers[2]}
+            </button>
             <hr/>
         </div>
     )
