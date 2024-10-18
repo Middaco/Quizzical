9aa5cb18ac2a1f5eff16ded3d69467de7c283a7c
Middaco <bogdan02081@gmail.com>
2024-10-18 10:26:33 +0300

    Questions and answers are being displayed. Further modifications are needed for shuffling the answers

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,10 +2,13 @@ export default function Question(props){
     return (
         <div className="question-container">
             <h2>{props.question}</h2>
-            <button className ="question-option">First option</button>
-            <button className ="question-option">Second option</button>
-            <button className ="question-option">Third option</button>
-            <button className ="question-option">Fourth option</button>
+
+            <button 
+                className ="question-option"
+            >{props.correct_answer}</button>
+            <button className ="question-option">{props.incorrect_answers[0]}</button>
+            <button className ="question-option">{props.incorrect_answers[1]}</button>
+            <button className ="question-option">{props.incorrect_answers[2]}</button>
             <hr/>
         </div>
     )
--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -5,24 +5,23 @@ export default function Quizz(){
     const [questions, setQuestions] = useState([])
 
     useEffect(() => {
-        console.log("Use effect run!")
         fetch('https://opentdb.com/api.php?amount=5&type=multiple')
             .then(response => response.json())
             .then(data => setQuestions(data.results))
-
         }, [])
-    console.log(questions)
 
     if (!questions.length) {
         return <h1>Loading questions...</h1>;
     }
-
+    console.log(questions)
     return (
         <div className="questions-wrapper">
             {questions.map((question, index) => 
                 <Question
                     key={index}
                     question={question.question}
+                    incorrect_answers={question.incorrect_answers}
+                    correct_answer={question.correct_answer}
                 />
             )}
         </div>
