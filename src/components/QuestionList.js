80ec2e1530d21eafd99a08f3a87bf5ad06becb94
Middaco <bogdan02081@gmail.com>
2024-10-21 17:02:25 +0300

    A state for the answers was also created in order to stop the reordering of the answer buttons every time an re-render ocurred

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,27 +1,15 @@
 import Question from './Question'
 
 export default function QuestionList(props){
-    {/**Create an usestate which will hold the information of each question
-        At the current state when a button is clicked and the 'show results'
-        button is pushed, Question is re-rendered with 'isSelected' as false
-        Having the question in useState should display the Questions properly */}
     return (
         <div className='question-wrapper'>
             {props.questions.map((question, index) => {
-                const answers = []
-                question.incorrect_answers.forEach((answer) => answers.push({
-                    answer: answer,
-                    isCorrect: false
-                }))
-                answers.splice(Math.floor(Math.random() * answers.length), 0, {
-                    answer: question.correct_answer,
-                    isCorrect: true
-                })
+                console.log(props.answers)
                 return (
                     <Question
                         key={index}
                         question={question.question}
-                        answers={answers}
+                        answers={props.answers[index]}
                         isSelected={false}
                         showResult={props.showResult}
                     />
--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 export default function Quizz(){
     const [questions, setQuestions] = useState([])
     const [showResult, setShowResult] = useState(false)
+    const [answers, setAnswers] = useState([])
 
     useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=5&type=multiple')
@@ -19,11 +20,39 @@ export default function Quizz(){
         return <h1>Loading questions...</h1>;
     }
 
+    //Define the answers state, in order to avoid re-rendering the answers and 
+    //not change the order of the Answers. Also this will help on keeping the 
+    //selected answer SELECTED
+    if(!answers.length){
+        console.log("Computing Answers")
+        const tempArrayOfArrays = []
+        let tempArrayOfAnswers = []
+        questions.forEach(question => {
+            question.incorrect_answers.forEach((answer) => tempArrayOfAnswers.push(
+                {
+                    answer: answer, 
+                    isCorrect: false, 
+                    isSelected: false
+                }
+            ))
+            tempArrayOfAnswers.splice(Math.floor(Math.random() * tempArrayOfAnswers.length), 0, {
+                answer: question.correct_answer,
+                isCorrect: true,
+                isSelected: false
+            })
+            tempArrayOfArrays.push(tempArrayOfAnswers)
+            tempArrayOfAnswers = []
+        })
+        setAnswers(tempArrayOfArrays)
+    }
+    
+
     return (
         <div className="quizz-container">
             {/**create a button to check the answers */}
             <QuestionList
                 questions={questions}
+                answers={answers}
                 showResult={showResult}
             />
             
